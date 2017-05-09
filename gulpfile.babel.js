3d22804f8cb71c4d7a1d953dfa559d2e99a11b44
Isao Yagi <isao.michael.yagi@ericsson.com>
2017-05-08 19:35:06 -0700

    Enable --minify and --sourcemaps options, rename stuff.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,36 +4,43 @@ import {env, log, PluginError} from 'gulp-util';
 import merge from 'merge2';
 import del from 'del'
 
+// Command line flags.
+const sourcemapsOn = !env.hasOwnProperty('sourcemaps') || env.sourcemaps;
+const minifyOn = env.minify;
+
+
 /*
-    typescript: transpile to es6, with definitions and source maps.
+    Transpile to es6, with type definitions, and source maps.
 */
 import typescript from 'gulp-typescript';
 import sourcemaps from 'gulp-sourcemaps';
-const tsProject = typescript.createProject('tsconfig.json');
 
+const tsProject = typescript.createProject('tsconfig.json');
 const sources = tsProject.config.include;
 const es6Out = tsProject.config.compilerOptions.outDir;
 const dtsOut = tsProject.config.compilerOptions.declarationDir;
-gulp.task('tsc', ['clean'], (cb) => {
+
+gulp.task('typescript', ['clean'], (cb) => {
     var tscErrorCount = 0;
 
     const tsResult = tsProject.src()
         .pipe(sourcemaps.init())
         .pipe(tsProject())
-        .on('error', (er) => new PluginError('tsc', er, {count: tscErrorCount++}))
+        .on('error', (er) => new PluginError('typescript', er, {count: tscErrorCount++}))
         .on('finish', () => tscErrorCount && process.exit(1));
 
     return merge([
         tsResult.dts
             .pipe(gulp.dest(dtsOut)),
         tsResult.js
-            .pipe(sourcemaps.write('.'))
+            .pipe(gulpIf(sourcemapsOn, sourcemaps.write('.')))
             .pipe(gulp.dest(es6Out)),
     ]);
 });
 
+
 /*
-    rollup: bundle es6 modules into a single file.
+    Bundle es6 modules into a single file.
 */
 import {rollup} from 'rollup';
 const {entry, destDir} = tsProject.config.gulpOptions;
@@ -42,28 +49,41 @@ const output = {
     format: 'es',
     sourceMap: true,
 };
-gulp.task('rollup', ['tsc'], (cb) => {
-    return rollup({entry}).then((bundle) => bundle.write(output));
-});
+gulp.task('bundle', ['typescript'],
+    (cb) => rollup({entry}).then((bundle) => bundle.write(output))
+);
+
 
 /*
-    transpile es6 to es5, and optionally compress it.
+    Transpile es6 to es5, and optionally compress it.
 */
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
-gulp.task('es5', ['rollup'], (cb) => {
+gulp.task('es5', ['bundle'], (cb) => {
     gulp.src(entry)
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(gulpIf(sourcemapsOn, sourcemaps.init({loadMaps: true})))
         .pipe(babel())
-        .pipe(gulpIf(env.compress, uglify()))
-        .pipe(sourcemaps.write('.'))
+        .pipe(gulpIf(minifyOn, uglify()))
+        .pipe(gulpIf(sourcemapsOn, sourcemaps.write('.')))
         .pipe(gulp.dest(destDir));
 });
 
 
 /*
-    clean, watch, default
+    clean, watch, default, help.
 */
 gulp.task('clean', () => del([destDir + '/*']));
-gulp.task('watch', ['rollup'], () => gulp.watch(sources, ['rollup']));
-gulp.task('default', ['rollup']);
+gulp.task('watch', ['bundle'], () => gulp.watch(sources, ['bundle']));
+gulp.task('default', ['bundle']);
+gulp.task('help', () => log(`
+Tasks (default is "bundle"):
+    typescript      Transpile Typescript to ES2015 (ES6).
+    bundle          Bundle ES2015 modules into a single file.
+    es5             Transpile ES2015 the bundle into ES5.
+
+Options:
+    --sourcemaps    Enable sourcemaps (enabled by default).
+    --minify        Compress the output bundle (disabled by default).
+
+The paths to the input and output files are in tsconfig.json.
+`));
